4b201eb54b99de8bf42f2f16a0ad9d4bdee324cb
Alessio <108763332+alessioVelluso@users.noreply.github.com>
2025-06-25 12:07:39 +0200

    Create users_data.js

---
+++ users_data.js
@@ -0,0 +1,110 @@
+export const data = [
+    { 
+        name: "Alessio", 
+        lvl: 1, 
+        online: true,
+        workingOn: [1, 2, 3, 4, 5]
+    },
+    { 
+        name: "Martina", 
+        lvl: 3, 
+        online: false,
+        workingOn: [2, 4, 6]
+    },
+    { 
+        name: "Luca", 
+        lvl: 2, 
+        online: true,
+        workingOn: [1, 3, 5, 7]
+    },
+    { 
+        name: "Giulia", 
+        lvl: 4, 
+        online: false,
+        workingOn: [3, 4, 5, 6]
+    },
+    { 
+        name: "Marco", 
+        lvl: 5, 
+        online: true,
+        workingOn: [1, 2, 3]
+    },
+    { 
+        name: "Sara", 
+        lvl: 2, 
+        online: true,
+        workingOn: [4, 5, 6, 7]
+    },
+    { 
+        name: "Davide", 
+        lvl: 3, 
+        online: false,
+        workingOn: [2, 3, 6]
+    },
+    { 
+        name: "Elena", 
+        lvl: 1, 
+        online: true,
+        workingOn: [1, 5, 7]
+    },
+    { 
+        name: "Francesca", 
+        lvl: 4, 
+        online: true,
+        workingOn: [2, 3, 4, 7]
+    },
+    { 
+        name: "Giorgio", 
+        lvl: 5, 
+        online: false,
+        workingOn: [1, 6, 7]
+    },
+    { 
+        name: "Valentina", 
+        lvl: 2, 
+        online: true,
+        workingOn: [3, 4, 5, 6]
+    },
+    { 
+        name: "Simone", 
+        lvl: 3, 
+        online: false,
+        workingOn: [1, 2, 4, 5, 7]
+    },
+    { 
+        name: "Chiara", 
+        lvl: 1, 
+        online: true,
+        workingOn: [2, 5, 6]
+    },
+    { 
+        name: "Andrea", 
+        lvl: 4, 
+        online: true,
+        workingOn: [1, 3, 5, 6]
+    },
+    { 
+        name: "Paolo", 
+        lvl: 2, 
+        online: false,
+        workingOn: [4, 5, 7]
+    },
+    { 
+        name: "Laura", 
+        lvl: 5, 
+        online: true,
+        workingOn: [1, 2, 3, 4, 6]
+    },
+    { 
+        name: "Federico", 
+        lvl: 3, 
+        online: true,
+        workingOn: [2, 4, 5, 7]
+    },
+    { 
+        name: "Marta", 
+        lvl: 1, 
+        online: false,
+        workingOn: [1, 3, 6]
+    }
+];
